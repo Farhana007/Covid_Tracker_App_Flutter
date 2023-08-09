63ded6cb61c7054ff79fe493a8f6b874fc89b2bd
Farhana Akter <106755038+Farhana007@users.noreply.github.com>
2023-08-09 11:35:13 -0400

    new changes added

--- lib/View/countries_list_screen.dart
+++ lib/View/countries_list_screen.dart
@@ -124,7 +124,27 @@ class _CountriesListScreenState extends State<CountriesListScreen> {
                                           MaterialPageRoute(
                                               builder: (context) =>
                                                   DetailScreen(
-                                                    //try calling response from api here 
+                                                    image: snapshot.data![index]
+                                                        ['countryInfo']['flag'],
+                                                    name: snapshot.data![index]
+                                                        ['country'],
+                                                    totalCases: snapshot
+                                                        .data![index]['cases'],
+                                                    totalRecovered:
+                                                        snapshot.data![index]
+                                                            ['recovered'],
+                                                    totalDeaths: snapshot
+                                                        .data![index]['deaths'],
+                                                    active: snapshot
+                                                        .data![index]['active'],
+                                                    test: snapshot.data![index]
+                                                        ['tests'],
+                                                    todayRecovered:
+                                                        snapshot.data![index]
+                                                            ['todayRecovered'],
+                                                    critical:
+                                                        snapshot.data![index]
+                                                            ['critical'], 
                                                     
                                                   )));
                                     },
